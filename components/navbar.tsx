aa3d50b4d4923c9e0d943159088b8379f1100b08
Zetah07 <sebastiancastro0792@gmail.com>
2023-06-30 13:25:54 -0500

    navigation bar ---> setting page in comming

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,28 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 import { MainNav } from "@/components/main-nav";
 import StoreSwitcher  from "@/components/store-switcher";
+import prismadb from "@/lib/prismadb";
 
-const Navbar = () => {
+const Navbar = async () => {
+    const { userId } = auth();
+
+    if (!userId) {
+        redirect("/sign-in");
+    };
+
+    const stores = await prismadb.store.findMany({
+        where: {
+            userId,
+        },
+    });
+
     return ( 
         <div className="border-b">
             <div className="flex h-16 items-center px-4">
                 <div>
-                    <StoreSwitcher items={[]} />
+                    <StoreSwitcher items={stores} />
                 </div>
                 <div>
                     <MainNav className="mx-6" />
